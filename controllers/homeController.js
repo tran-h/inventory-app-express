bfe781c2d3b50e8eea1e0f075f2e68b5a13cfcfc
tran-h <43885150+tran-h@users.noreply.github.com>
2025-07-27 10:59:54 -0400

    feat: connect database

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,15 +1,15 @@
-// const db = require("../db");
+const db = require("../db");
 
 exports.home = async (req, res) => {
   try {
-    // const result = await db.query("SELECT * FROM genres ORDER BY name");
-    const result = {
-      rows: [
-        { id: 1, name: "Action" },
-        { id: 2, name: "Adventure" },
-        { id: 3, name: "RPG" },
-      ],
-    };
+    const result = await db.query("SELECT * FROM genres ORDER BY name");
+    // const result = {
+    //   rows: [
+    //     { id: 1, name: "Action" },
+    //     { id: 2, name: "Adventure" },
+    //     { id: 3, name: "RPG" },
+    //   ],
+    // };
     res.render("home", { genres: result.rows });
   } catch (err) {
     console.error(err);
