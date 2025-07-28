0d40c6534048e5b5381d2f148a4fc4d1ea266f0d
tran-h <43885150+tran-h@users.noreply.github.com>
2025-07-27 22:47:00 -0400

    feat: add header, footer, and styling

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require('path');
 const homeRoutes = require('./routes/home');
 
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homeRoutes);
 
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,13 +3,6 @@ const db = require("../db");
 exports.home = async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM genres ORDER BY name");
-    // const result = {
-    //   rows: [
-    //     { id: 1, name: "Action" },
-    //     { id: 2, name: "Adventure" },
-    //     { id: 3, name: "RPG" },
-    //   ],
-    // };
     res.render("home", { genres: result.rows });
   } catch (err) {
     console.error(err);
