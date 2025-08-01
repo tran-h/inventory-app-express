6decea8a62e1851e05376232c112c663bb8f08d7
tran-h <43885150+tran-h@users.noreply.github.com>
2025-08-01 14:53:19 -0400

    feat: update database logic to use local or remote db

--- db.js
+++ db.js
@@ -1,6 +1,17 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL_LOCAL,
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = isProduction
+  ? process.env.DATABASE_URL
+  : process.env.DATABASE_URL_LOCAL;
+
+const pool = new Pool({
+  connectionString,
+  ssl: isProduction
+    ? { rejectUnauthorized: false } // required for Render
+    : false, // disable SSL locally
 });
+
+module.exports = pool;
